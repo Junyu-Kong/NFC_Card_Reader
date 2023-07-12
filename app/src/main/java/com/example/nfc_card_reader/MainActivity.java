25c36ceec5d49f771ab44a98fc11f536ac1d1b96
Junyu Kong <junyukong@g.ucla.edu>
2023-07-12 00:28:27 -0700

    parseText added

--- app/src/main/java/com/example/nfc_card_reader/MainActivity.java
+++ app/src/main/java/com/example/nfc_card_reader/MainActivity.java
@@ -85,29 +85,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void handleIntent(Intent intent) {
-        Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-        if (messages != null){
-            txt2.setText("not null");
-            for (Parcelable message : messages){
-                NdefMessage ndefMessage = (NdefMessage) message;
-                for (NdefRecord record: ndefMessage.getRecords()) {
-                    switch(record.getTnf()){
-                        case NdefRecord.TNF_WELL_KNOWN:
-                            txt2.setText("Well Known: ");
-                            if (Arrays.equals(record.getType(), NdefRecord.RTD_TEXT)){
-                                txt2.append("Text:");
-                                txt2.append(new String(record.getPayload()));
-                            }
-                            else if (Arrays.equals(record.getType(), NdefRecord.RTD_URI)){
-                                txt2.append("URI:");
-                                txt2.append(new String(record.getPayload()));
-                            }
-                    }
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                    NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage msgs[] = null;
+            int contentSize = 0;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                    contentSize += msgs[i].toByteArray().length;
                 }
             }
+            try {
+                if (msgs != null) {
+                    NdefRecord record = msgs[0].getRecords()[0];
+                    String textRecord = parseTextRecord(record);
+                    txt2.setText(textRecord + "\n\ntext\n" + contentSize + " bytes");
+                }
+            } catch (Exception e) {
+            }
         }
-        else {
-            txt2.setText("null");
+    }
+
+    public static String parseTextRecord(NdefRecord ndefRecord) {
+        /**
+         * 判断数据是否为NDEF格式
+         */
+        //判断TNF
+        if (ndefRecord.getTnf() != NdefRecord.TNF_WELL_KNOWN) {
+            return null;
+        }
+        //判断可变的长度的类型
+        if (!Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+            return null;
+        }
+        try {
+            //获得字节数组，然后进行分析
+            byte[] payload = ndefRecord.getPayload();
+            //下面开始NDEF文本数据第一个字节，状态字节
+            //判断文本是基于UTF-8还是UTF-16的，取第一个字节"位与"上16进制的80，16进制的80也就是最高位是1，
+            //其他位都是0，所以进行"位与"运算后就会保留最高位
+            String textEncoding = ((payload[0] & 0x80) == 0) ? "UTF-8" : "UTF-16";
+            //3f最高两位是0，第六位是1，所以进行"位与"运算后获得第六位
+            int languageCodeLength = payload[0] & 0x3f;
+            //下面开始NDEF文本数据第二个字节，语言编码
+            //获得语言编码
+            String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            //下面开始NDEF文本数据后面的字节，解析出文本
+            String textRecord = new String(payload, languageCodeLength + 1,
+                    payload.length - languageCodeLength - 1, textEncoding);
+            return textRecord;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
         }
     }
 }
